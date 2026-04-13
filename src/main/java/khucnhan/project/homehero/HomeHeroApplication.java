ad34af37b1f17039e1bfbcfa440ee0030c45ef6d
Khuc Nhan <khucvii@gmail.com>
2026-04-13 15:52:50 +0700

    create project

---
+++ src/main/java/khucnhan/project/homehero/HomeHeroApplication.java
@@ -0,0 +1,13 @@
+package khucnhan.project.homehero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeHeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeHeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/khucnhan/project/homehero/HomeHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package khucnhan.project.homehero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeHeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
